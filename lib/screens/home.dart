e21881354bdf6ee124c73bf06b6ff80b3189d4dd
Nick Conklin <nconkl1@outlook.com>
2022-05-27 15:39:36 -0400

    updated home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maple_tracker/widgets/app_bar_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key, required this.title}) : super(key: key);
@@ -7,43 +8,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        actions: const [
-          Icon(Icons.settings),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.orange,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            ListTile(
-              leading: Icon(Icons.view_day_outlined),
-              title: Text('Dailies'),
-            ),
-            ListTile(
-              leading: Icon(Icons.view_week_outlined),
-              title: Text('Weeklies'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return AppBarDrawer(title: title);
   }
 }
