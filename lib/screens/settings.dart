002ffa8bd1bdd6c7180f33e52586da136afe7234
Nick Conklin <nconkl1@outlook.com>
2022-05-27 15:38:40 -0400

    added settings screen

---
+++ lib/screens/settings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
